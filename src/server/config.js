5a9ee8ef4fbb61377993a862b9f2d1506acc419c
jl781 <junbo.li@duke.edu>
2018-04-25 02:06:12 -0400

    add mongodb

--- src/server/config.js
+++
@@ -1,3 +0,0 @@
-// when deployed, use locally defined values, otherwise use typical settings for server
-export const HOST = process.env.HOST || '127.0.0.1'
-export const PORT = process.env.PORT || 3000
---
+++ src/server/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+/* Connect to db*/
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/events");
+var schema = new mongoose.Schema(
+    { 'eid': Number,
+      'title': String,
+      'location': String,
+      'time': String
+});
+
+var temp = mongoose.model('events', schema);
+
+/* Add to db*/
+// new temp({
+//     'eid': 3,
+//     'title': 'Web Final',
+//     'location': 'UNC',
+//     'time': 'Tomorrow'
+// }).save(function(err, doc) {
+//     console.log(doc);
+// })
+
+
+/* Find from db*/
+var result;
+temp.find(function(err, doc) {
+    // console.log(doc);
+    result = doc;
+})
+
+/* Remove from db*/
+// temp.remove({"eid": 3}, function(err){
+//     console.log(err);
+// });
+
+/* Update from db*/
+temp.update({"eid": 0}, {$set:{"title": "5v5 Basketball"}}, function(err, raw){
+    console.log(raw);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send(JSON.stringify(result));
+  res.end();
+});
+
+module.exports = router;
