567f01d5e3f8536fe8ff9e1d4f12164611ea5a36
jl781 <junbo.li@duke.edu>
2018-04-24 21:38:42 -0400

    little fix

---
+++ src/server/config.js
@@ -0,0 +1,3 @@
+// when deployed, use locally defined values, otherwise use typical settings for server
+export const HOST = process.env.HOST || '127.0.0.1'
+export const PORT = process.env.PORT || 3000
