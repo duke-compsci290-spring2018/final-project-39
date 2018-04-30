3d633c3e84bd186ddf7171b6a99480cc0d2ae2d2
jl781 <junbo.li@duke.edu>
2018-04-30 04:17:12 -0400

    change server

--- src/client/assets/secrets.js
+++ src/client/assets/secrets.js
@@ -1,3 +1,3 @@
 // generally keep passwords, API keys, other secrets separate from the main code
 export const MAP_KEY = 'AIzaSyDECQZj67sb0tObghTyerAksjEoISCrzZM'
-export const SERVER_URL = 'https://final-project-whats-happening.herokuapp.com'
+export const SERVER_URL = 'https://guarded-dusk-56726.herokuapp.com'
